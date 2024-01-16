de307363ec41a82d97b4cc7dcafa09417fe59864
Dmitriy-O <dimaovsannikov2@gmail.com>
2024-01-16 12:58:34 +0200

    Initial commit

---
+++ src/test/java/org/byovsiannikov/sticky_notes/StickyNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.byovsiannikov.sticky_notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StickyNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
