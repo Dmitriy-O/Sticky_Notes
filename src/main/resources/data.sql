03cc6ecd5577c249a76c0db6c36b902353ab36b8
Dmitriy-O <dimaovsannikov2@gmail.com>
2024-02-03 18:06:50 +0200

    Spring Security v0.9 Docker-compose Postgres + Flyway

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
-insert into t_user(id,c_username,c_password)
-values (1,'j.jamson','{noop}password');
-
-insert into t_user_authority(id_user,c_authorities)
-values (1,'ROLE_MANAGER');
+-- insert into t_user(id,c_username,c_password)
+-- values (1,'j.jamson','{noop}password');
+--
+-- insert into t_user_authority(id_user,c_authorities)
+-- values (1,'ROLE_MANAGER');
