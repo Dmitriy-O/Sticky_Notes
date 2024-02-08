14878c1241b8cb54dae23cd5d06c917318ad7e0e
Dmitriy-O <dimaovsannikov2@gmail.com>
2024-02-08 22:26:19 +0200

    Spring Security v0.9

--- src/main/java/org/byovsiannikov/sticky_notes/dto/JWTRegitsterDTO.java
+++ src/main/java/org/byovsiannikov/sticky_notes/dto/JWTRegitsterDTO.java
@@ -1,13 +1,60 @@
 package org.byovsiannikov.sticky_notes.dto;
 
-import lombok.Builder;
 import lombok.Data;
 
 @Data
-@Builder
 public class JWTRegitsterDTO {
     private String userName;
     private String email;
     private String password;
     private String passwordConfirm;
+
+    JWTRegitsterDTO(String userName, String email, String password, String passwordConfirm) {
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+        this.passwordConfirm = passwordConfirm;
+    }
+
+    public static JWTRegitsterDTOBuilder builder() {
+        return new JWTRegitsterDTOBuilder();
+    }
+
+    public static class JWTRegitsterDTOBuilder {
+        private String userName;
+        private String email;
+        private String password;
+        private String passwordConfirm;
+
+        JWTRegitsterDTOBuilder() {
+        }
+
+        public JWTRegitsterDTOBuilder userName(String userName) {
+            this.userName = userName;
+            return this;
+        }
+
+        public JWTRegitsterDTOBuilder email(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public JWTRegitsterDTOBuilder password(String password) {
+            this.password = password;
+            return this;
+        }
+
+        public JWTRegitsterDTOBuilder passwordConfirm(String passwordConfirm) {
+            this.passwordConfirm = passwordConfirm;
+            return this;
+        }
+
+        public JWTRegitsterDTO build() {
+            return new JWTRegitsterDTO(this.userName, this.email, this.password, this.passwordConfirm);
+        }
+
+        public String toString() {
+            return "JWTRegitsterDTO.JWTRegitsterDTOBuilder(userName=" + this.userName + ", email=" + this.email + ", password=" + this.password + ", passwordConfirm=" + this.passwordConfirm + ")";
+        }
+    }
 }
